6d4aaf711f99ef83d46534d308fa969971bd5334
simfor <sf9@princeton.edu>
2018-11-14 09:07:25 -0500

    Added precision arguments to the functions readInfo and readBinary The precision for the centroid data is not consistent across autotracker versions. Not sure if there's an automatic way of dealing with this. For now, I added arguments to let the user change the precision if needed

--- R/utils.R
+++ R/utils.R
@@ -19,14 +19,16 @@ lmp <- function (modelobject) {
 
 .trak <- setClass(Class = "trak", slots = c(speed = "data.frame", speed.regressed = "data.frame", time = 'numeric', centroid = "data.frame",  activity = "list", metadata = "data.frame", hz = "numeric"), package = 'RATrak')
 
-readInfo <- function(speedBinFileName = NULL, centroidBinFileName = NULL, timeBinFileName = NULL, metadataFileName, wellCount, start = 1, end = wellCount, hz = 5, inferPhenos = T){
+readInfo <- function(speedBinFileName = NULL, centroidBinFileName = NULL, timeBinFileName = NULL, metadataFileName, wellCount, start = 1, end = wellCount, hz = 5, inferPhenos = F, size.centroid = NA_integer_){
+  #WARNING: The precision for the centroid data is not consistent across autotracker versions. If the centroid coordinates make no sense, try changing size.centroid
+  #This is passed on as the size argument to readBin()
   time <- numeric()
   centroid <- data.frame()
   speed <- data.frame()
   
   if(!is.null(centroidBinFileName) & !is.null(timeBinFileName) & !is.null(speedBinFileName)){  
     speed <- readBinary(speedBinFileName, wellCount, dataType = 'speed')
-    centroid <- readBinary(centroidBinFileName, wellCount, dataType = 'centroid')
+    centroid <- readBinary(centroidBinFileName, wellCount, dataType = 'centroid', size.centroid = size.centroid)
     time <- readBinary(timeBinFileName, dataType = 'time')
     
     centroid <- centroid[2:nrow(centroid), ] #Align speed and centroid data
@@ -37,13 +39,13 @@ readInfo <- function(speedBinFileName = NULL, centroidBinFileName = NULL, timeBi
   }
   else if(!is.null(centroidBinFileName) & is.null(timeBinFileName) & !is.null(speedBinFileName)){  
     speed <- readBinary(speedBinFileName, wellCount, dataType = 'speed')
-    centroid <- readBinary(centroidBinFileName, wellCount, dataType = 'centroid')
+    centroid <- readBinary(centroidBinFileName, wellCount, dataType = 'centroid', size.centroid = size.centroid)
     
     centroid <- centroid[2:nrow(centroid), ] #Align speed and centroid data
   }
   else if(!is.null(centroidBinFileName) & !is.null(timeBinFileName) & is.null(speedBinFileName)){
     print('Only centroid and time data provided. Calculating speed')
-    centroid <- readBinary(centroidBinFileName, wellCount, dataType = 'centroid')
+    centroid <- readBinary(centroidBinFileName, wellCount, dataType = 'centroid', size.centroid = size.centroid)
     time <- readBinary(timeBinFileName, dataType = 'time')
     speed <- flies.calculateSpeed(as.matrix(centroid), time)
     
@@ -58,7 +60,7 @@ readInfo <- function(speedBinFileName = NULL, centroidBinFileName = NULL, timeBi
   }
   else if(!is.null(centroidBinFileName) & is.null(timeBinFileName) & is.null(speedBinFileName)){
     print('Only centroid data provided. Calculating speed')
-    centroid <- readBinary(centroidBinFileName, wellCount, dataType = 'centroid')
+    centroid <- readBinary(centroidBinFileName, wellCount, dataType = 'centroid', size.centroid = size.centroid)
     speed <- flies.calculateSpeed(as.matrix(centroid))*5 #Rescale speed to pixel/s
     
     centroid <- centroid[2:nrow(centroid), ] #Align speed and centroid data
@@ -73,16 +75,17 @@ readInfo <- function(speedBinFileName = NULL, centroidBinFileName = NULL, timeBi
   return(data)
 }
 
-readBinary <- function(fileName, colCount, dataType){
+readBinary <- function(fileName, colCount, dataType, size.centroid = NA_integer_, size.speed_time = 4){
+  #WARNING: The precision for the centroid data has been changed between single and double in different autotracker versions. 
   file <- file(fileName, "rb")
   if(dataType == 'speed'){
-    mat <- matrix(readBin(file, numeric(), n = 1e8, size = 4), ncol = colCount, byrow = TRUE)
+    mat <- matrix(readBin(file, numeric(), n = 1e9, size = size.speed_time), ncol = colCount, byrow = TRUE)
     mat <- mat[4:nrow(mat), ] #Discard first few frames
     close(file)
     return(as.data.frame(mat))
   }
   else if(dataType == 'centroid'){
-    mat.tmp <- matrix(readBin(file, numeric(), n = 1e8, size = 8), ncol = colCount*2, byrow = TRUE)
+    mat.tmp <- matrix(readBin(file, numeric(), n = 1e9, size = size.centroid), ncol = colCount*2, byrow = TRUE)
     #Reshape matrix
     mat <- matrix(ncol = ncol(mat.tmp), nrow = nrow(mat.tmp))
     xCols <- seq(from = 1, to = ncol(mat.tmp) - 1, by = 2)
@@ -94,7 +97,7 @@ readBinary <- function(fileName, colCount, dataType){
     return(as.data.frame(mat))
   }
   else if(dataType == 'time'){
-    time <- readBin(file, numeric(), n = 1e8, size = 4)
+    time <- readBin(file, numeric(), n = 1e9, size = size.speed_time)
     time <- time[4:length(time)] #Discard first few frames
     close(file)
     return(time)
